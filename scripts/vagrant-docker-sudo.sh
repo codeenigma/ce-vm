56798361ba71f08c6d05c2ee9f56f8135b1d58ae
Pascal Morin <pascal.morin@codeenigma.com>
2018-02-07 12:45:01 +0100

    Add a wrapper for Docker (sudo) on Linux
    # Conflicts:
    #	.gitignore

---
+++ scripts/vagrant-docker-sudo.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Wrapper script (Linux host only). Allows Vagrant to issue Docker commands
+# without having to permanently add your user to the "docker" group.
+#
+# To use it:
+# - Make sure it is executable:
+#     chmod +x ~/.CodeEnigma/ce-vm/ce-vm-upstream/scripts/vagrant-docker-sudo.sh
+# - Add it as an alias in your bashrc, zshrc, ... eg:
+#     alias vg="$HOME/.CodeEnigma/ce-vm/ce-vm-upstream/scripts/vagrant-docker-sudo.sh"
+# 
+# You will then be able to call "vg up", "vg ssh" as your normal user.
+# Note that you will still be prompted for your 'sudoer' password, this is
+# needed to add you temporarily to the "docker" group.
+
+# Check if we are running as root.
+if [ "$(id -u)" -lt "1" ]; then
+   echo "This script must not be run as root. You will get prompted for sudo access during the process."
+   exit 1
+fi
+
+# Get own path.
+OWN=$(readlink "$0");
+if [ -z "$OWN" ]; then
+ OWN="$0"
+fi
+OWN_DIR=$( cd "$( dirname "$OWN" )" && pwd -P)
+CURRENT_CALL_DIR=$(pwd -P)
+VAGRANT_WRAPPER_CMD="/bin/sh $OWN_DIR/vagrant-wrapper.sh"
+echo $VAGRANT_WRAPPER_CMD
+ORIG_USER="$USER"
+sudo gpasswd -a "$ORIG_USER" "docker"
+sudo su - -l "$ORIG_USER" -c "cd $CURRENT_CALL_DIR && $VAGRANT_WRAPPER_CMD $@" 
+sudo gpasswd -d "$ORIG_USER" "docker"
