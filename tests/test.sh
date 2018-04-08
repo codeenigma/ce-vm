edfa12d31e19f9953a494f626135fd5f210f6006
Pascal <pascal.morin@codeenigma.com>
2018-04-08 20:11:56 +0200

    Remove Mac support, due to https://github.com/travis-ci/docs-travis-ci-com/pull/1185

--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# Keep current dir in mind to know where to move back when done.
+OWN=$(readlink "$0");
+if [ -z "$OWN" ]; then
+ OWN="$0"
+fi
+OWN_DIR=$( cd "$( dirname "$OWN" )" && pwd -P)
+
 TESTS="drupal"
 for TEST in $TESTS; do
-  cd $TEST
+  cd $OWN_DIR/$TEST
   curl -O https://raw.githubusercontent.com/codeenigma/ce-vm-model/5.x/ce-vm/Vagrantfile
   vagrant up
 done
