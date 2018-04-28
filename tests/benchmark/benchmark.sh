d34eb32d6ecffdf4e75df622f40d11893f2a1219
pascal <pascal.morin@codeenigma.com>
2018-04-28 11:24:43 +0200

    Adding draft benchmark

---
+++ tests/benchmark/benchmark.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Keep current dir in mind to know where to move back when done.
+OWN=$(readlink "$0");
+if [ -z "$OWN" ]; then
+ OWN="$0"
+fi
+OWN_DIR=$( cd "$( dirname "$OWN" )" && pwd -P)
+
+# Define global config.
+BUILD_DIR="$OWN_DIR/build"
+RESULT_DIR="$OWN_DIR/results"
+RESULT_FILE="$RESULT_DIR/$1.csv"
+VAGRANTFILE="$OWN_DIR/Vagrantfile"
+CONFIG_YML="$OWN_DIR/config.yml"
+VOL_TYPES="native sshfs unison"
+
+# Clean existing build if it exists.
+if [ -d "$BUILD_DIR" ]; then
+  sudo rm -rf "$BUILD_DIR"
+fi
+# Clean existing results file.
+if [ -f "$RESULT_FILE" ]; then
+  sudo rm "$RESULT_FILE"
+fi
+
+# Include project specifics.
+# shellcheck source=./write-mount.sh
+. "$OWN_DIR/$1.sh"
+
+# Prepare environment.
+mkdir "$BUILD_DIR"
+prepare_test
+
+for PASS in `seq 1 $2`; do
+  run_test
+done
+
+cleanup_test
+
+if [ -d "$BUILD_DIR" ]; then
+  sudo rm -rf "$BUILD_DIR"
+fi
---
+++ tests/benchmark/write-mount.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+prepare_test(){
+  COUNTER=10
+  LINE=""
+  for VOL_TYPE in $VOL_TYPES; do
+    COUNTER=$((COUNTER + 1))
+    RUN_DIR="$BUILD_DIR/$VOL_TYPE"
+    VM_DIR="$RUN_DIR/ce-vm"
+    mkdir -p "$VM_DIR"
+    cp "$VAGRANTFILE" "$VM_DIR/"
+    cp "$CONFIG_YML" "$VM_DIR/"
+    echo "volume_type: $VOL_TYPE" >> "$VM_DIR/config.yml"
+    echo "" >> "$VM_DIR/config.yml"
+    echo "project_name: $VOL_TYPE" >> "$VM_DIR/config.yml"
+    echo "" >> "$VM_DIR/config.yml" >> "$VM_DIR/config.yml"
+    echo "net_base: 192.168.$COUNTER" >> "$VM_DIR/config.yml"
+    cd "$VM_DIR"
+    vagrant up log || exit 1
+    LINE="$VOL_TYPE,$LINE"
+  done
+  echo "$LINE" >> $RESULT_FILE
+}
+
+run_test(){
+  LINE=""
+  for VOL_TYPE in $VOL_TYPES; do
+    RUN_DIR="$BUILD_DIR/$VOL_TYPE"
+    VM_DIR="$RUN_DIR/ce-vm"
+    cd "$VM_DIR"
+    START_TIME=`date +%s`
+    vagrant ssh -c "cd /vagrant &&  composer create-project symfony/website-skeleton my-project"
+    END_TIME=`date +%s`
+    RUN_TIME=$((END_TIME-START_TIME))
+    LINE="$RUN_TIME,$LINE"
+  done
+  echo "$LINE" >> $RESULT_FILE  
+}
+
+cleanup_test(){
+  for VOL_TYPE in $VOL_TYPES; do
+    RUN_DIR="$BUILD_DIR/$VOL_TYPE"
+    VM_DIR="$RUN_DIR/ce-vm"
+    cd "$VM_DIR"
+    vagrant destroy --force
+  done
+}
