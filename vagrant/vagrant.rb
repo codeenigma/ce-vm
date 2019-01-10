b79f49f3e631daa72a739accd0ba6097f815f11d
Pascal Morin <pascal.morin@codeenigma.com>
2019-01-10 20:41:44 +0100

    Fix plugin install (#67)
    * Fix plugin install
    
    * Switch version
    
    * Use new version for base images
    
    * Missing a WP test
    
    * Fix Platform mismatch for SSH port forwarding

--- vagrant/vagrant.rb
+++ vagrant/vagrant.rb
@@ -14,7 +14,7 @@ ENV['VAGRANT_DOTFILE_PATH'] = '.vagrant' if ENV['VAGRANT_DOTFILE_PATH'].nil?
 # We currently use only one plugin, so skip iterating.
 def vagrant_ensure_plugins
   plugin = 'vagrant-hostsupdater'
-  return unless config_get_item('skip_vagrant_plugins_check') == true
+  return if config_get_item('skip_vagrant_plugins_check') == true
   system "vagrant plugin install #{plugin}" unless Vagrant.has_plugin? plugin.to_s
 end
 
